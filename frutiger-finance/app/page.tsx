7ad8ab69e7a4fb95d851a9ecad6eab2ddc03cc81
mendezc0251 <cmendez0251@gmail.com>
2025-05-06 20:59:22 -0500

    Added exit animations

--- frutiger-finance/app/page.tsx
+++ frutiger-finance/app/page.tsx
@@ -1,7 +1,7 @@
 import Greeting from "@/components/Greeting";
 import Button from "@/components/Button";
 import '@/styles/page.css'
-import Begin from "./finance";
+import Begin from "@/components/finance";
 
 export default function Home() {
   const expenses = [{id: 1, name: "McDonalds", amount: 16.54, category: "Food", reoccuring: false}, {id: 2, name: "Mortgage", amount: 879.65, category: "Housing", reoccuring: true}, {id: 3, name: "Credit Card Payment", amount: 30.76, category: "Debt", reoccuring: false}]
---
+++ frutiger-finance/components/finance.tsx
@@ -0,0 +1,42 @@
+"use client"
+import Button from "@/components/Button";
+import { AnimatePresence, motion } from "motion/react"
+import {useState} from "react"
+
+export default function Begin() {
+  const [isVisible, setIsVisible] = useState(true)
+  return (
+            <div>
+                <AnimatePresence initial={false}>
+                  {isVisible ? (<motion.h1 className="home--title">
+                  <span style={{color: '#2fa7b9'}}>F</span>
+                  <span style={{color: '#3dc9dc'}}>r</span>
+                  <span style={{color: '#2fa7b9'}}>u</span>
+                  <span style={{color: '#3dc9dc'}}>t</span>
+                  <span style={{color: '#2fa7b9'}}>i</span>
+                  <span style={{color: '#3dc9dc'}}>g</span>
+                  <span style={{color: '#2fa7b9'}}>e</span>
+                  <span style={{color: '#3dc9dc'}}>r </span>
+                  <span style={{color: '#2fa7b9'}}>F</span>
+                  <span style={{color: '#3dc9dc'}}>i</span>
+                  <span style={{color: '#2fa7b9'}}>n</span>
+                  <span style={{color: '#3dc9dc'}}>a</span>
+                  <span style={{color: '#2fa7b9'}}>n</span>
+                  <span style={{color: '#3dc9dc'}}>c</span>
+                  <span style={{color: '#2fa7b9'}}>e</span>
+                  </motion.h1>) : null}
+                </AnimatePresence>
+                <AnimatePresence initial={false}>
+                  {isVisible ? (<motion.div className="home--button">
+                    <Button 
+                    text="Begin!" 
+                    color="#3dc9dc" 
+                    hcolor="#2fa7b9" 
+                    height={100} 
+                    width={200} 
+                    font={65}/>
+                  </motion.div>) : null}
+                </AnimatePresence>
+            </div>
+  );
+}
